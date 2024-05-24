e9da1372a031a3ed2125d4f93c57abbe6e732493
varshiniyarra16 <155799893+varshiniyarra16@users.noreply.github.com>
2024-05-24 18:45:39 +0530

    GitHub

---
+++ github/file2.py
@@ -0,0 +1,17 @@
+class calc:
+    def init(self,a,b):
+        self.x=a
+        self.y=b
+    def add(a,b):
+        print(a+b)
+    def subtract(a,b):
+        print(a-b)
+    def product(a,b):
+        print(a*b)
+        
+obj=calc
+a=int(input())
+b=int(input())
+obj.add(a,b)
+obj.subtract(a,b)
+obj.product(a,b) 
